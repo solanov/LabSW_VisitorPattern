b864a58b0fea8f7414945cfa359d82c016acdf98
Vinz Solano <bamharu228@gmail.com>
2026-03-30 01:11:11 +0000

    Renamed classes to follow the main class as reference

--- TelcoSubscription.java
+++ TelcoSubscription.java
@@ -1,33 +1,9 @@
-public class TelcoSubscription implements Telco{
-    private String telcoName;
-    private double promoPrice;
-    private int dataAllowance;    
-    private boolean unliCallText;
-
-    public TelcoSubscription(int dataAllowance, double promoPrice, String telcoName, boolean unliCallText){
-        this.dataAllowance = dataAllowance;
-        this.promoPrice = promoPrice;
-        this.telcoName = telcoName;
-        this.unliCallText = unliCallText;
-    }
-
-    @Override
-    public String accept(UsagePromo promo, double price) {
-        return promo.showAllowance(this.telcoName, price);
-    }
-
-    @Override
-    public String accept(UnliCallTextOffer unliPackage, boolean unliCallText) {
-        return unliPackage.showUnliCallsTextOffer(this.telcoName, unliCallText);
-    }
-
-    // Getters and Setters
-    public void setDataAllowance(int dataAllowance){ this.dataAllowance = dataAllowance; }
-    public int getDataAllowance(){return dataAllowance;}
-    public void setPromoPrice(double promoPrice){this.promoPrice = promoPrice;}
-    public double getPromoPrice(){return promoPrice;}
-    public void setTelcoName(String telcoName){this.telcoName = telcoName;}
-    public String getTelcoName(){return telcoName;}
-    public void setUnliCallText(boolean unliCallText){this.unliCallText = unliCallText;}
-    public boolean getUnliCallText(){return unliCallText;}
+public interface TelcoSubscription {
+    String accept(UsagePromo promo, double price);
+    String accept(UnliCallTextOffer unliPackage, boolean unliCallText);
+    
+    // Necessary for your main class to access these via the interface
+    String getTelcoName();
+    double getPromoPrice();
+    boolean getUnliCallText();
 }
